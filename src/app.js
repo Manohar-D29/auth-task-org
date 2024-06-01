523786f066a5eaeb6321efe71160557d249fc54f
Manohar-29 <manohar.dewoolkar97@gmail.com>
2024-06-01 13:35:58 +0530

    modify default route

--- src/app.js
+++ src/app.js
@@ -9,10 +9,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // health check route
-app.get('/health-check', (req, res) => {
+app.get('/', (req, res) => {
     res.status(200).json({
-        status: 'ok',
-        message: 'Checking health'
+        success: true,
+        message: 'Hello from Server 👍...!'
     })
 })
 
